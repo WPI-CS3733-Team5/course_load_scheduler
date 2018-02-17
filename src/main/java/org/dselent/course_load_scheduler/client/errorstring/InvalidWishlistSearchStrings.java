5530edbc37e1b37de02accae52b798f3c6ac535d
MT-Grim <mtuckergrim@gmail.com>
2018-02-17 14:14:48 -0500

    ## committed by Tucker

---
+++ src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidWishlistSearchStrings.java
@@ -0,0 +1,9 @@
+package org.dselent.course_load_scheduler.client.errorstring;
+
+public class InvalidWishlistSearchStrings {
+	
+	public static final String INVALID_SEARCH_BUTTON= "Please input a valid search key.";
+	public static final String VIEW_COURSE_DETAILS_BUTTON = "Could not view course.";
+	public static final String EDIT_WISHLIST_BUTTON = "Could not edit wishlist. Sorry.";
+	public static final String REQUEST_NEW_SCHEDULE_BUTTON = "Could not get new schedule.";
+}
