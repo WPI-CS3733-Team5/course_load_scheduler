c0a94d551fe5f35b942c8fe98049ce72c6ee9f82
MT-Grim <mtuckergrim@gmail.com>
2018-02-24 17:58:20 -0500

    ## committed by Tucker

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/HomePresenterImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/HomePresenterImpl.java
@@ -83,10 +83,13 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 	
 	@Override
 	public void go(HasWidgets container) {
+		//gets the active user off parentPresenter and checks if his role is admin or not
+		//if the user is a regular user then clear the dropdown and add only this user to the list
 		if(parentPresenter.getActiveUser().getUserRole() == 1) {
 			view.getUserDropDown().clear();
 			view.getUserDropDown().addItem(parentPresenter.getActiveUser().getUserName());
 		} else
+		//if the user isnt the regular user then fill the dropdown menu with all the users in userInfoList
 		{
 			view.getUserDropDown().clear();
 			for(UserInfo i: userInfoList) {
@@ -94,6 +97,7 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 			}
 		}
 		
+		//puts the A, B, C ect, terms in the drop down menu
 		view.getTermDropDown().addItem("A");
 		view.getTermDropDown().addItem("B");
 		view.getTermDropDown().addItem("C");
@@ -120,6 +124,9 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 	public void setParentPresenter(IndexPresenter parentPresenter) {
 		this.parentPresenter = parentPresenter;
 	}
+	
+	//apply filter when the termBox and userBox are filled in.
+	// show the load screen while stuff is happening
 	@Override
 	public void applyFilter(ListBox termBox, ListBox userBox) {
 		if(!applyFilterInProgress)
@@ -134,11 +141,15 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 		}
 	}
 	
+	//apply the filters when given a term, and a username. Filter by term and username. 
 	private boolean applyFilter(String termString, String username)
 	{
 		parentPresenter.showLoadScreen();
 		
+		//make termString an int and set term = to it.
 		int term = termToInt(termString);
+		
+		//create a new user and instructor, then set instructors Id to 0
 		UserInfo user = new UserInfo();
 		InstructorInfo instructor = new InstructorInfo();
 		instructor.setId(0);
@@ -146,9 +157,12 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 		ArrayList<SectionInfo> sections = new ArrayList<>();
 		ArrayList<LabInfo> labs = new ArrayList<>();
 		
+		//makes two lists that associate the given info with eachother. 
 		ArrayList<Pair<Pair<CourseInfo, SectionInfo>, CalendarInfo>> coursesAndSectionsAndCalendars = new ArrayList<>();
 		ArrayList<Pair<Pair<CourseInfo, LabInfo>, CalendarInfo>> coursesAndLabsAndCalendars = new ArrayList<>();
 		
+		//runs until it hits the end of the userInfoList(see above). checks if a user in the userInfoList shares a 
+		//userName with the username parameter. If it finds on it sets the user variable from above the the found user.
 		for(int i = 0; i < userInfoList.size(); i++) {
 			if(userInfoList.get(i).getUserName().equals(username)) {
 				user = userInfoList.get(i);
@@ -156,6 +170,7 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 			}
 		}
 		
+		//same thing as the username loop above but for the instructor variable. 
 		for(int i = 0; i < instructorInfoList.size(); i++) {
 			if(instructorInfoList.get(i).getUserInfoId() == user.getId()) {
 				instructor = instructorInfoList.get(i);
@@ -163,14 +178,22 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 			}
 		}
 		
+		//if the instructor variables Id = 0 then add that string to the views calendarList and return false. 
 		if(instructor.getId() == 0) {
 			view.getCalendarList().addItem("The selected user has no schedule");
 			return false;
 		}
 		
+		//iterate over the scheduleLinksList
 		for(int i = 0; i < scheduleLinksList.size(); i++) {
+			//if the ith element in in scheduleLinksList has a instructorInfoId = to the instructor 
+			//variable from above then continue
+			//the loop. else break from the loop.
 			if(scheduleLinksList.get(i).getInstructorInfoId() == instructor.getId()) {
+				//iterate over the sectionInfo list. 
 				for(int b = 0; b < sectionInfoList.size(); b++) {
+					//if the ith element of scheduleLinksList' id is the same as the bth element of sectionInfoList
+					//then add the bth element of sectionInfoList to the sections list. then break from the loop.
 					if(scheduleLinksList.get(i).getSectionInfoId() == sectionInfoList.get(b).getId()) {
 						sections.add(sectionInfoList.get(b));
 						break;
@@ -180,20 +203,30 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 			break;
 		}
 		
+		//iterate over the labInfoList.
 		for(int i = 0; i < labInfoList.size(); i++) {
+			//if the ith element in labInfoList has the same Id as instructor, then add that ith lab to the labs list. 
 			if(labInfoList.get(i).getInstructorInfoId() == instructor.getId()) {
 				labs.add(labInfoList.get(i));
 			}
 		}
 		
-		
+		//iterate over the section list.
 		for(int i = 0; i < sections.size(); i++) {
+			//iterate over the courseInfList.
 			for(int b = 0; b < courseInfoList.size(); b++) {
+				//if the bth element of courseInfoList has the same id as the ith element of sections,
 				if(courseInfoList.get(b).getId() == sections.get(i).getCourseInfoId()) {
+					//then iterate over calendarInfoList.
 					for(int c = 0; c < calendarInfoList.size(); c++) {
+						//if the cth element of calendarInfoList's Id is the same as the ith element of section's calendarInfoId
+						//and calendarInfoList term property has the same term value as the term variable made from the parameter 
 						if(calendarInfoList.get(c).getId() == sections.get(i).getCalendarInfoId() && calendarInfoList.get(c).getTerm() == term) {
+							//then set CourseInfo and SectionInfo to be associated with eachother in a pair. else break from loop.
 							Pair<CourseInfo, SectionInfo> tempPair1 = new Pair(courseInfoList.get(b), sections.get(i));
+							//then set that pair and calendarInfo to be associated with each other. 
 							Pair<Pair<CourseInfo, SectionInfo>, CalendarInfo> temp2 = new Pair(tempPair1, calendarInfoList.get(c));
+							//then add those 3 associated lists to coursesAndSectionsAndCalendars, and break from the for loop
 							coursesAndSectionsAndCalendars.add(temp2);
 							break;
 						}
@@ -203,12 +236,19 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 			}
 		}
 		
+		//iterate over sections 
 		for(int i = 0; i < sections.size(); i++) {
+			//iterate over labs
 			for(int b = 0; b < labs.size(); b ++) {
+				//if the bth element of labs has the same id as the ith element of section then 
 				if(labs.get(b).getSectionInfoId() == sections.get(i).getId()) {
+					//iterate over courseInfoList. else break from the loop. 
 					for(int c = 0; c < courseInfoList.size(); c++) {
+						//if the cth element of courseInfiList has the same id as teh ith element of sections then
 						if(courseInfoList.get(c).getId() == sections.get(i).getCourseInfoId()) {
+							//iterate over calendarInfoList
 							for(int d = 0; d < calendarInfoList.size(); d++) {
+								// same thing as the big for loop above, just with different lists, and info
 								if(calendarInfoList.get(d).getId() == labs.get(b).getCalendarInfoId() && calendarInfoList.get(d).getTerm() == term) {
 									Pair<CourseInfo, LabInfo> temp1 = new Pair(courseInfoList.get(c), labs.get(b));
 									Pair<Pair<CourseInfo, LabInfo>, CalendarInfo> temp2= new Pair(temp1, calendarInfoList.get(d));
@@ -224,10 +264,14 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 			}
 		}
 		
+		//iterate over our combined list.
 		for(int i = 0; i < coursesAndSectionsAndCalendars.size(); i++) {
+			//add the courseName, from the first value of the first pair of the ith element of the courseAndSectionsAndCaldendars paired list. 
 			view.getCalendarList().addItem(coursesAndSectionsAndCalendars.get(i).getValue1().getValue1().getCourseName() + 
 					" Section Number: " + 
+					//set the second value of the ith element of this lists term to a string. 
 					termToString(coursesAndSectionsAndCalendars.get(i).getValue2().getTerm()) + 
+					
 					coursesAndSectionsAndCalendars.get(i).getValue1().getValue2().getSectionNumber() + 
 					coursesAndSectionsAndCalendars.get(i).getValue2().getYear() + 
 					" Schedule: " +
@@ -237,6 +281,8 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 					" - " +
 					coursesAndSectionsAndCalendars.get(i).getValue2().getEndTime());
 		}
+		
+		//same thing as aboce
 		for(int i = 0; i < coursesAndLabsAndCalendars.size(); i++) {
 			view.getCalendarList().addItem(coursesAndLabsAndCalendars.get(i).getValue1().getValue1().getCourseName() +
 					" Lab: " +
@@ -250,6 +296,7 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 					coursesAndLabsAndCalendars.get(i).getValue2().getEndTime());
 		}
 		
+		//turns the load screen off. load the actual screen. 
 		parentPresenter.hideLoadScreen();
 		applyFilterInProgress = false;
 		view.getApply().setEnabled(true);
@@ -257,6 +304,7 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 		return true;
 	}
 	
+	//turn the term string into an int. 
 	private int termToInt(String termString) {
 		
 		int term = 0;
@@ -271,6 +319,7 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 		return term;
 	}
 	
+	//turn the term int into a string. 
 	private String termToString (Integer termInt) {
 		String term = "ERROR";
 
@@ -297,6 +346,7 @@ public class HomePresenterImpl extends BasePresenterImpl implements HomePresente
 		
 		return term;
 	}
+	
 	
 	@Override
 	public void acceptSchedule(ListBox termBox, ListBox userBox) {
