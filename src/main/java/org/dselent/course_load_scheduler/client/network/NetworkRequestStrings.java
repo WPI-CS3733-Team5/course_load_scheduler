dd3aedeaeb6a35ab6a6ac7b7b551d923c980e231
Jeremy <jmhoffman@wpi.edu>
2018-02-25 21:39:22 -0500

    ## Commit by Jeremy

--- src/main/java/org/dselent/course_load_scheduler/client/network/NetworkRequestStrings.java
+++ src/main/java/org/dselent/course_load_scheduler/client/network/NetworkRequestStrings.java
@@ -6,7 +6,7 @@ public final class NetworkRequestStrings
 	public static final String BASE_REQUEST = "course_load_scheduler/";
 	public static final String LOGIN = "userInfo/login";
 	public static final String GET_ALL_USER_AND_INSTRUCTOR_INFO = "userInfo/getAll";
-	public static final String GET_ONE_USER_AND_INSTRUCTOR_INFO = "userInfo/GetOneUser";
+	public static final String GET_ONE_USER_AND_INSTRUCTOR_INFO = "userInfo/getOneUser";
 	public static final String GET_ALL_COURSE_SECTION_LAB_CALENDAR_INFO = "courses/getAllCourses";
 	public static final String GET_ALL_USER_INSTRUCTOR_COURSE_SECTION_LAB_CALENDAR_SCHEDULE_INFO = "userInfo/theBigOne";
 	public static final String GET_ALL_WISHLIST_SECTIONS_FOR_USER = "sectionInfo/getAllWishlistSectionGivenUser";
