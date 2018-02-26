e1f45c04ff614b49ee6a3de7aef848bc65cca14e
Benjamin Secino <bjsecino@wpi.edu>
2018-02-25 21:34:48 -0500

    #Ben changed login request to /LOGIN

--- src/main/java/org/dselent/course_load_scheduler/client/network/NetworkRequestStrings.java
+++ src/main/java/org/dselent/course_load_scheduler/client/network/NetworkRequestStrings.java
@@ -4,7 +4,7 @@ public final class NetworkRequestStrings
 {
 	public static final String SERVER_LOCATION = "http://localhost:8080/";
 	public static final String BASE_REQUEST = "course_load_scheduler/";
-	public static final String LOGIN = "userInfo/GetOneUser";
+	public static final String LOGIN = "userInfo/login";
 	public static final String GET_ALL_USER_AND_INSTRUCTOR_INFO = "userInfo/getAll";
 	public static final String GET_ONE_USER_AND_INSTRUCTOR_INFO = "userInfo/GetOneUser";
 	public static final String GET_ALL_COURSE_SECTION_LAB_CALENDAR_INFO = "courses/getAllCourses";
